4e5cababa89fe7588a5ca43a1b0defed51f78807
mixedfeelings <george@wietor.com>
2022-05-09 03:29:23 -0400

    z-index up header so it rides above floating images

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="sticky flex top-0 w-full h-12 px-4 bg-white border-b-2 border-b-black  items-center md:h-14"
+    class="sticky flex top-0 w-full h-12 px-4 bg-white border-b-2 border-b-black  items-center md:h-14 z-10"
   >
     <Hamburger class="md:hidden mr-4" />
     <Logo />
