510aad5a1363ee8fc4db9337f2d0e194d13612e2
mixedfeelings <george@wietor.com>
2022-05-19 04:32:50 -0400

    tweak collection header style

--- components/CollectionHeader/CollectionHeader.vue
+++ components/CollectionHeader/CollectionHeader.vue
@@ -1,9 +1,7 @@
 <template>
-  <div
-    class="flex flex-col py-8 justify-center"
-  >
-    <h1 class="text-3xl font-bold font-serif text-center">{{ title }}</h1>
-    <div v-if="description" v-html="description" class="text-sm mt-1 text-center"></div>
+  <div class="collection-header" >
+    <h1>{{ title }}</h1>
+    <div v-if="description" v-html="description" class="description "></div>
   </div>
 </template>
 
@@ -13,3 +11,22 @@ const props = defineProps<{
   description: string;
 }>();
 </script>
+
+<style>
+
+.collection-header {
+  @apply flex flex-col py-8 justify-center;
+}
+
+.collection-header h1 {
+  @apply text-3xl font-bold font-serif text-center;
+
+}
+.collection-header .description {
+  @apply leading-relaxed text-sm mt-2 text-center;
+}
+
+.collection-header .description a {
+ @apply underline ;
+}
+</style>
