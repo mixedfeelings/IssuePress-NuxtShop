f16b4751e5b4d20eca0b66b08d45a16dda40b826
mixedfeelings <george@wietor.com>
2022-05-22 05:09:18 -0400

    add artists list

---
+++ apollo/queries/collections.ts
@@ -0,0 +1,50 @@
+import { gql } from "graphql-tag";
+
+export const collections = gql`
+    query collection(
+      $numCollections: Int,
+      $cursor: String,
+    ) {
+      collections(
+          first: $numCollections,
+          after: $cursor,
+          sortKey: TITLE,   
+      ) {
+        edges {
+          cursor
+          node {
+            id
+            title
+            description
+            handle
+            artist: metafield(namespace: "issuepress", key: "artistCollection") {
+                value
+                type
+            }
+            products(first: 1) {
+                edges {
+                  cursor
+                  node {
+                    images(first: 1) {
+                      edges {
+                        node {
+                          height
+                          id
+                          url
+                          width
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            endCursor
+          }
+      }
+    }
+`;
--- pages/artists/index.vue
+++ pages/artists/index.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>
+  <section>
     <Html>
       <Head>
         <Title>Artists | Issue Press</Title>
         <Meta name="description" content="Lorem ipsum" />
       </Head>
     </Html>
-    <div class="container mx-auto my-8 text-center">Add readme here</div>
-  </div>
+    <div v-if="collectionList" class="py-6">
+        <div class="collection-header">
+          <h1>{{filtered_collections.length}} Artists</h1>
+        </div>
+
+        <ProductGrid class="gap-y-8">
+          <NuxtLink v-for="(collection) in filtered_collections" class="card w-1/2 md:w-1/3 lg:w-1/4 text-center" :to="`/artists/${collection.node.handle}`">
+            <div class="card-image-wrapper" >
+              <div class="card-image-inner">
+                <img
+                  alt="alt"
+                  height="auto"
+                  loading="lazy'"
+                  width="auto"
+                  :src="collection.node.products.edges[0].node.images?.edges[0]?.node?.url"
+                />
+              </div>
+            </div>
+            <div class="card-body">
+              <h3 class="font-serif">{{collection.node.title}}</h3>
+            </div>
+          </NuxtLink>
+        </ProductGrid>
+    </div>
+    <div v-if="error" >{{error}}</div>
+  </section>
 </template>
+
+<script setup lang="ts">
+import { useQuery, useResult } from "@vue/apollo-composable";
+import { collections } from "~/apollo/queries/collections";
+import { useColorStore } from "~/stores/colors";
+import ProductGrid from "~/components/ProductGrid/ProductGrid.vue";
+
+const colorStore = useColorStore();
+
+const { result, error, fetchMore} = useQuery(collections, { numCollections: 100, cursor: null });
+const collectionList = useResult(result, null, (data) => data.collections?.edges);
+const filtered_collections = computed(() => collectionList.value.filter(item => item.node.artist));
+const hasNextPage = computed(() => result.value.collections.pageInfo.hasNextPage )
+
+
+onMounted(() => {
+  colorStore.setGlobalColor();
+  document.documentElement.style.setProperty('--global-color',`var(--color-${colorStore.globalColor})`);
+});
+</script>
