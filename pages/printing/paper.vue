4faf846105982f5cf93df88387123aebdcd3b836
mixedfeelings <george@wietor.com>
2022-06-07 03:22:47 -0400

    add content to the paper page

--- pages/printing/paper.vue
+++ pages/printing/paper.vue
@@ -2,11 +2,110 @@
     <section>
         <Html>
             <Head>
-                <Title>Paper Options | Issue Press</Title>
+                <Title>Paper | Issue Press</Title>
+                <Meta name="description" content="Learn more about papers for Risograph printing."/>
             </Head>
         </Html>
         <PrintDocs>
-            <h2 class="text-2xl font-serif pb-6">Paper Options</h2>
+            <h2 class="text-3xl font-serif pb-6">Paper</h2>
+
+            <p>***<br/>
+            A quick word about paper supplies in 2022: Things are bleak for the paper market all across North America at the moment. Because of a massive pulp (the raw material of paper) shortage – likely a delayed effect of manufacturing slowdowns during the COVID-19 pandemic – many, many papers are unavailable or only available sporadically. I will do my best to source papers for any project, but it is worth noting that this is a universal issue at the moment with no clear end in sight.
+            <br/>***
+            </p>            
+            <h3 class="text-xl font-serif py-4 ">Choosing Paper</h3>
+            <p>Because the stencil printing process uses liquid vegetable-based ink, Risograph digital duplicators can only use uncoated paper that allows it to fully absorb and dry. Below are some key factors to consider when selecting paper for a project, and some recommended stocks known for their success in Risograph printing. </p>
+            <p>Coated, metallic, and hologram paper are not available for stencil printing.</p>
+
+            <h4 class="text-lg font-serif pt-6 pb-2">Paper Weight</h4>
+            Paper thickness in North America is measured in <em>basis weight</em>: the weight of a ream of paper, in pounds (#'s'), at its parent sheet size. There are many types of paper each with their own basis weight. It's honestly confusing, and best not to think about too much. Just know that that higher the number, the thicker the paper. Risograph printers can comfortably print on papers between 20# bond and 80# cover.  
+            <ul class="list-disc list-outside ml-4">
+                <li class="py-1 pl-2"><strong>Cover</strong> - Thick paper, used for publication covers and prints.</li>
+                <li class="py-1 pl-2"><strong>Bristol</strong> - Although similar to cover, a bristol is classified differently as it is technically an uncoated "paperboard" because of the way it is created.</li>
+                <li class="py-1 pl-2"><strong>Text</strong> - Standard thinner stock used for zines, book pages, flyers, and folded prints. Usually available at 50#, 70#, and 80# - 70# is the most typical. </li>
+                <li class="py-1 pl-2"><strong>Bond</strong> - At 20#, this is your typical familiar copy machine paper. The lightest paper we offer, but also has the highest <em>dot gain</em> (the spread of ink as it sets in the paper). Honestly, I don't use this much. </li>
+            </ul>
+            <h4 class="text-lg font-serif pt-6 pb-2">Finish</h4>
+            <p>Uncoated paper is available in many paper finishes, but I have found that Vellum and Smooth work the best in most applications. They are both great for stencil printing, but also both have their own benefits and drawbacks.</p>
+            <ul class="list-disc list-outside ml-4">
+                <li class="py-1 pl-2"><strong>Vellum</strong> - A rough, bulky finish that works really well for projects with a lot of ink because it dries to the touch more quickly - but can also <em>potentially</em> take much longer to completely set than smooth paper. The toothiness of the paper can also <em>potentially</em> lend it self to splotchier prints and dot gain than smooth feel. The roughness of the finish gives the paper a great "tactile" feel. </li>
+                <li class="py-1 pl-2"><strong>Smooth</strong> - Smooth paper is, well, smooth, and is great for work that needs a brigher, more intricate print. It is slower to dry than vellum paper, and can therefore be prone to set off (the wet ink of a print marking the back of the next sheet). Despite this, I have found that it can dry more thoroughly in the long run.</li>
+            </ul>
+            
+
+            <h3 class="text-2xl font-serif py-4 mt-6">Some Recommended Papers Stencil Printing</h3>
+            <h4 class="text-lg font-serif pb-4 underline"><a href="https://frenchpaper.com" target="_blank">French Paper Co.</a></h4>
+            <p class="italic text-xs pb-2">TL;DR: Top-tier, recycled, locally made, "artisinal," expensive!</p>
+            <p>In Michigan, we are lucky to have one of the best Family-run paper mills in the United States. Located in Niles, just about 90 minutes South of the Issue Press studio, the French family has milled fine uncoated paper for 5 generations.</p>
+            <p>Most French Paper Co. papers are suitable for stencil printing, but I have had particular success with the following lines:</p>
+            <AccordionPanel title="Speckletone">
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">70# text / 80# cover | Vellum</div>
+                </template>
+                <p>Speckle-tone is characterized by it's namesake speckles and shives and availability in 50 shades of beige. That was a joke, but there are a lot of browns ¯\_(ツ)_/¯.</p> 
+                <a class="underline" href="https://www.frenchpaper.com/collections/speckletone" target="_blank">Website</a>            
+            </AccordionPanel>
+            <AccordionPanel title="Construction">
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">70# text / 80# cover | Vellum</div>
+                </template>
+                <p>Despite the name, it is not the super absorbant craft construction paper you remember from childhood, but is very well-suited for stencil printing. Insulation Pink is easily one of my favorite papers ever.</p>
+                <a class="underline" href="https://www.frenchpaper.com/collections/construction" target="_blank">Website</a>
+            </AccordionPanel>
+            <AccordionPanel title="Pop-Tone">
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">70# text / 65# cover | Vellum</div>
+                </template>
+                <p>Recycled paper with a vellum finish. Bright colors, lots of great pinks, purples, and blues.</p>
+                <a class="underline" href="https://www.frenchpaper.com/collections/pop-tone" target="_blank">Website</a>
+            </AccordionPanel>
+            <AccordionPanel title="Glo-Tone Neon">
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">60# text / 65# cover | Vellum</div>
+                </template>
+                <p>French's brightest paper. Like eye-popping</p>
+                <a class="underline" href="https://www.frenchpaper.com/collections/glo-tone" target="_blank">Website</a>
+            </AccordionPanel>
+            <AccordionPanel title="Dur-O-Tone">
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">60# text / 65# cover | Vellum</div>
+                </template>                
+                <p>Duro-tone is a weird one. It feels like French's catch-all for whatever doesn't fit in their other lines That said, there are some excellent papers for stencil printing and some others that are less great. Among the papers in the Duro-tone collection, I highly reccomend all of the newsprints and Packing Brown Wrap. I do not reccomend the butcher papers for stencil printing. Too smooth, too waxy. </p>
+                <a class="underline" href="https://www.frenchpaper.com/collections/dur-o-tone" target="_blank">Website</a>
+            </AccordionPanel>
+             <AccordionPanel title="Kraft-Tone">
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">70# text | Vellum</div>
+                </template>   
+                <p>The newest line. Some pretty great papers for riso. Unfortunately, the cover weights only come in 100#. Get it together, French Paper Co.!</p>  
+                <a class="underline" href="https://www.frenchpaper.com/collections/kraft-tone" target="_blank">Website</a>
+            </AccordionPanel>
+            <p></p>
+            <p></p>
+            <h3 class="text-xl font-serif pb-4 underline"><a href="https://www.domtar.com/en" target="_blank">Domtar Paper</a></h3>
+            <p class="italic text-xs">TLDR: high-quality, often some recycled content, budget-friendly</p>
+            <p>While Domtar no longer operates mills in Michigan (😢), this Americaan / Canadian paper company still mills many of its paper products here in the Great Lakes region. It is reccommended for budget-conscious projects that still require a high quality stock that works well with stencil printing. Fun fact: Domtar used to run a gypsum mine here in Grand Rapids, a spooky tour of which looms heavily on my grade school experience.</p>             
+            <AccordionPanel title="Cougar">                
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">70#, 80# text / 80# cover | Smooth & Vellum | Acid-free</div>
+                </template>   
+                <p>Domtar's flagship paper. Comes in any color you like, as long as it is white or natural (kind of a creamy off-white). Cougar Smooth has more or less become my house paper, and I use it on many projects.</p>         
+                <a class="underline" href="https://www.domtar.com/en/what-we-make/paper/printing-and-publishing-papers/digital-printing-papers/cougar-digital" target="_blank">Website</a>
+            </AccordionPanel>
+             <AccordionPanel title="Lettermark Vellum Bristol">                
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">67# bristol | Vellum</div>
+                </template>   
+                <p>The vellum finish cover equivalent to Lettermark Colors Opaque</p>         
+                <a class="underline" href="https://www.domtar.com/sites/default/files/2022-03/LMColors-VB-Spec%2BStocking-EN.pdf" target="_blank">Spec Sheet</a>
+            </AccordionPanel>
+             <AccordionPanel title="Lettermark Colors Opaque">                
+                <template v-slot:post-title>
+                    <div class="text-xs text-mono pt-1">60#, 70 text | Vellum</div>
+                </template>   
+                <p>The standard pastel rainbow of office paper. White is not available at text weight.</p>         
+                <a class="underline" href="https://www.domtar.com/sites/default/files/2022-03/LMColors-Opaque-Spec%2BStocking-EN_0.pdf" target="_blank">Spec Sheet</a>
+            </AccordionPanel>  
         </PrintDocs>
     </section>
 
