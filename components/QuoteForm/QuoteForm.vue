45a322a93387647f591fbd709b212f023263db56
mixedfeelings <george@wietor.com>
2022-06-15 19:46:23 -0400

    botfield test

--- components/QuoteForm/QuoteForm.vue
+++ components/QuoteForm/QuoteForm.vue
@@ -148,7 +148,7 @@
 </template>
 <script setup lang="ts">
     import { ref } from 'vue';
-    import { InkList, ProjectTypeOptions, QuantityOptions, PrintSizeOptions, PublicationSizeOptions, CornerRadiiOptions, BindingTypeOptions, StapleColorOptions, DeliveryTypeOptions }  from "~/constants"
+    import { InkList, ProjectTypeOptions, QuantityOptions, PrintSizeOptions, PublicationSizeOptions, CornerRadiiOptions, BindingTypeOptions, StapleColorOptions, DeliveryTypeOptions } from "~/constants"
     import ConfettiExplosion from "vue-confetti-explosion";
 
     const errors = ref([]);
@@ -422,8 +422,8 @@
             .then(() => this.SuccessMessage = "Thank you! Your Quote request has been submitted!")
             .catch((err) => this.SuccessMessage = `Error: %s ${err}`)
             .finally(() => {
-                console.log("quote: %s", JSON.stringify(this.formData))
-                console.log(this.encode(this.formData))
+                // console.log("quote: %s", JSON.stringify(this.formData))
+                // console.log(this.encode(this.formData))
                 window.scrollTo(0,0);
                 explode();
 
